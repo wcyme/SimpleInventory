589f38ccc6a11fc4c75305039d1d68d831074ee5
johnny <wcy_me@hotmail.com>
2020-10-16 22:14:16 +0800

    Generate Import  Csv File Component

---
+++ src/main/webapp/app/import-csv-files/import-csv-files.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-import-csv-files',
+  templateUrl: './import-csv-files.component.html',
+  styleUrls: ['./import-csv-files.component.scss'],
+})
+export class ImportCsvFilesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
